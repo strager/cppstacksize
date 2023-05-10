2f4875d95a3bca58a19ff9fd120c67d7191f6f02
Matthew Glazar <strager.nds@gmail.com>
2023-05-09 19:12:16 -0700

    feat(test): add a COFF file with interesting debug info

---
+++ test/coff/primitives.cpp
@@ -0,0 +1,20 @@
+void primitives() {
+    unsigned char uc = 0;
+    unsigned short us = 0;
+    unsigned int ui = 0;
+    unsigned long ul = 0;
+    unsigned long long ull = 0;
+
+    signed char sc = 0;
+    signed short ss = 0;
+    signed int si = 0;
+    signed long sl = 0;
+    signed long long sll = 0;
+
+    char c = 'x';
+    wchar_t wc = L'x';
+
+    float f = 0.0f;
+    double d = 0.0;
+    long double ld = 0.0;
+}
