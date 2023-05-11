cf889af6e8ac57cb4e6e462c69335d5fc9da6fd0
Matthew Glazar <strager.nds@gmail.com>
2023-05-10 22:02:04 -0700

    feat(test): add some sample programs

---
+++ test/coff/multiple-functions.cpp
@@ -0,0 +1,4 @@
+void a() { }
+void b() { }
+void c() { a(); b(); }
+void d() { c(); }
---
+++ test/coff/primitive-parameters.cpp
@@ -0,0 +1,20 @@
+void primitiveParameters(
+    unsigned char uc,
+    unsigned short us,
+    unsigned int ui,
+    unsigned long ul,
+    unsigned long long ull,
+
+    signed char sc,
+    signed short ss,
+    signed int si,
+    signed long sl,
+    signed long long sll,
+
+    char c,
+    wchar_t wc,
+
+    float f,
+    double d,
+    long double ld) {
+}
