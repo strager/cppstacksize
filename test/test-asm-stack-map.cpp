11f5163118722c6a2e0a716e1bd8e3baafaacfb0
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-05-25 23:07:55 -0700

    refactor(test): use custom formatting to avoid Clang Format ugliness

--- test/test-asm-stack-map.cpp
+++ test/test-asm-stack-map.cpp
@@ -29,75 +29,73 @@ TEST(Test_ASM_Stack_Map, function_not_touching_stack) {
 }
 
 TEST(Test_ASM_Stack_Map, rsp_relative_store_touches) {
-  CHECK_TOUCHES(ASM_X86_64(
-                    // @asm-begin x86_64
-                    0x48, 0x89, 0x44, 0x24, 0x30,  // @asm mov %rax, 0x30(%rsp)
-                    ),                             // @asm-end
-                {
-                    Stack_Map_Touch(0, 0x30, 8),
-                });
+  // clang-format off
 
   CHECK_TOUCHES(ASM_X86_64(
-                    // @asm-begin x86_64
-                    // clang-format off
-                    0x48, 0xc7, 0x44, 0x24, 0x30, 0x45, 0x00, 0x00, 0x00,  // @asm movq $69, 0x30(%rsp)
-                    // clang-format on
-                    ),  // @asm-end
-                {
-                    Stack_Map_Touch(0, 0x30, 8),
-                });
+    // @asm-begin x86_64
+    0x48, 0x89, 0x44, 0x24, 0x30,  // @asm mov %rax, 0x30(%rsp)
+    // @asm-end
+  ), {
+    Stack_Map_Touch(0, 0x30, 8),
+  });
 
   CHECK_TOUCHES(ASM_X86_64(
-                    // @asm-begin x86_64
-                    0x89, 0x44, 0x24, 0x20,  // @asm mov %eax, 0x20(%rsp)
-                    ),                       // @asm-end
-                {
-                    Stack_Map_Touch(0, 0x20, 4),
-                });
+    // @asm-begin x86_64
+    0x48, 0xc7, 0x44, 0x24, 0x30, 0x45, 0x00, 0x00, 0x00,  // @asm movq $69, 0x30(%rsp)
+    // @asm-end
+  ), {
+    Stack_Map_Touch(0, 0x30, 8),
+  });
 
   CHECK_TOUCHES(ASM_X86_64(
-                    // @asm-begin x86_64
-                    // clang-format off
-                    0xc7, 0x44, 0x24, 0x20, 0x45, 0x00, 0x00, 0x00,  // @asm movl $69, 0x20(%rsp)
-                    // clang-format on
-                    ),  // @asm-end
-                {
-                    Stack_Map_Touch(0, 0x20, 4),
-                });
+    // @asm-begin x86_64
+    0x89, 0x44, 0x24, 0x20,  // @asm mov %eax, 0x20(%rsp)
+    // @asm-end
+  ), {
+    Stack_Map_Touch(0, 0x20, 4),
+  });
 
   CHECK_TOUCHES(ASM_X86_64(
-                    // @asm-begin x86_64
-                    0x66, 0x89, 0x44, 0x24, 0x20,  // @asm mov %ax, 0x20(%rsp)
-                    ),                             // @asm-end
-                {
-                    Stack_Map_Touch(0, 0x20, 2),
-                });
+    // @asm-begin x86_64
+    0xc7, 0x44, 0x24, 0x20, 0x45, 0x00, 0x00, 0x00,  // @asm movl $69, 0x20(%rsp)
+    // @asm-end
+  ), {
+    Stack_Map_Touch(0, 0x20, 4),
+  });
 
   CHECK_TOUCHES(ASM_X86_64(
-                    // @asm-begin x86_64
-                    // clang-format off
-                    0x66, 0xc7, 0x44, 0x24, 0x20, 0x45, 0x00,  // @asm movw $69, 0x20(%rsp)
-                    // clang-format on
-                    ),  // @asm-end
-                {
-                    Stack_Map_Touch(0, 0x20, 2),
-                });
+    // @asm-begin x86_64
+    0x66, 0x89, 0x44, 0x24, 0x20,  // @asm mov %ax, 0x20(%rsp)
+    // @asm-end
+  ), {
+    Stack_Map_Touch(0, 0x20, 2),
+  });
 
   CHECK_TOUCHES(ASM_X86_64(
-                    // @asm-begin x86_64
-                    0x88, 0x64, 0x24, 0x20,  // @asm mov %ah, 0x20(%rsp)
-                    ),                       // @asm-end
-                {
-                    Stack_Map_Touch(0, 0x20, 1),
-                });
+    // @asm-begin x86_64
+    0x66, 0xc7, 0x44, 0x24, 0x20, 0x45, 0x00,  // @asm movw $69, 0x20(%rsp)
+    // @asm-end
+  ), {
+    Stack_Map_Touch(0, 0x20, 2),
+  });
 
   CHECK_TOUCHES(ASM_X86_64(
-                    // @asm-begin x86_64
-                    0xc6, 0x44, 0x24, 0x20, 0x45,  // @asm movb $69, 0x20(%rsp)
-                    ),                             // @asm-end
-                {
-                    Stack_Map_Touch(0, 0x20, 1),
-                });
+    // @asm-begin x86_64
+    0x88, 0x64, 0x24, 0x20,  // @asm mov %ah, 0x20(%rsp)
+    // @asm-end
+  ), {
+    Stack_Map_Touch(0, 0x20, 1),
+  });
+
+  CHECK_TOUCHES(ASM_X86_64(
+    // @asm-begin x86_64
+    0xc6, 0x44, 0x24, 0x20, 0x45,  // @asm movb $69, 0x20(%rsp)
+    // @asm-end
+  ), {
+    Stack_Map_Touch(0, 0x20, 1),
+  });
+
+  // clang-format on
 }
 }
 }
