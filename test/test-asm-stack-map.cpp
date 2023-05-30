fb006bf4a51aa74507b1fee92f19d94a7eb0e57d
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-05-30 02:44:51 -0700

    refactor(asm-stack-map): submit to clang-format

--- test/test-asm-stack-map.cpp
+++ test/test-asm-stack-map.cpp
@@ -25,263 +25,193 @@ TEST(Test_ASM_Stack_Map, function_not_touching_stack) {
 }
 
 TEST(Test_ASM_Stack_Map, rsp_relative_store_touches) {
-  // clang-format off
+  CHECK_TOUCHES(ASM_X86_64("mov %rax, 0x30(%rsp)"),
+                {
+                    Stack_Map_Touch::write(0, 0x30, 8),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "mov %rax, 0x30(%rsp)"
-  ), {
-    Stack_Map_Touch::write(0, 0x30, 8),
-  });
+  CHECK_TOUCHES(ASM_X86_64("movq $69, 0x30(%rsp)"),
+                {
+                    Stack_Map_Touch::write(0, 0x30, 8),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "movq $69, 0x30(%rsp)"
-  ), {
-    Stack_Map_Touch::write(0, 0x30, 8),
-  });
+  CHECK_TOUCHES(ASM_X86_64("mov %eax, 0x20(%rsp)"),
+                {
+                    Stack_Map_Touch::write(0, 0x20, 4),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "mov %eax, 0x20(%rsp)"
-  ), {
-    Stack_Map_Touch::write(0, 0x20, 4),
-  });
+  CHECK_TOUCHES(ASM_X86_64("movl $69, 0x20(%rsp)"),
+                {
+                    Stack_Map_Touch::write(0, 0x20, 4),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "movl $69, 0x20(%rsp)"
-  ), {
-    Stack_Map_Touch::write(0, 0x20, 4),
-  });
+  CHECK_TOUCHES(ASM_X86_64("mov %ax, 0x20(%rsp)"),
+                {
+                    Stack_Map_Touch::write(0, 0x20, 2),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "mov %ax, 0x20(%rsp)"
-  ), {
-    Stack_Map_Touch::write(0, 0x20, 2),
-  });
+  CHECK_TOUCHES(ASM_X86_64("movw $69, 0x20(%rsp)"),
+                {
+                    Stack_Map_Touch::write(0, 0x20, 2),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "movw $69, 0x20(%rsp)"
-  ), {
-    Stack_Map_Touch::write(0, 0x20, 2),
-  });
+  CHECK_TOUCHES(ASM_X86_64("mov %ah, 0x20(%rsp)"),
+                {
+                    Stack_Map_Touch::write(0, 0x20, 1),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "mov %ah, 0x20(%rsp)"
-  ), {
-    Stack_Map_Touch::write(0, 0x20, 1),
-  });
-
-  CHECK_TOUCHES(ASM_X86_64(
-    "movb $69, 0x20(%rsp)"
-  ), {
-    Stack_Map_Touch::write(0, 0x20, 1),
-  });
-
-  // clang-format on
+  CHECK_TOUCHES(ASM_X86_64("movb $69, 0x20(%rsp)"),
+                {
+                    Stack_Map_Touch::write(0, 0x20, 1),
+                });
 }
 
 TEST(Test_ASM_Stack_Map, rsp_relative_load_touches) {
-  // clang-format off
+  CHECK_TOUCHES(ASM_X86_64("mov 0x30(%rsp), %rax"),
+                {
+                    Stack_Map_Touch::read(0, 0x30, 8),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "mov 0x30(%rsp), %rax"
-  ), {
-    Stack_Map_Touch::read(0, 0x30, 8),
-  });
+  CHECK_TOUCHES(ASM_X86_64("mov 0x20(%rsp), %eax"),
+                {
+                    Stack_Map_Touch::read(0, 0x20, 4),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "mov 0x20(%rsp), %eax"
-  ), {
-    Stack_Map_Touch::read(0, 0x20, 4),
-  });
+  CHECK_TOUCHES(ASM_X86_64("mov 0x20(%rsp), %ax"),
+                {
+                    Stack_Map_Touch::read(0, 0x20, 2),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "mov 0x20(%rsp), %ax"
-  ), {
-    Stack_Map_Touch::read(0, 0x20, 2),
-  });
+  CHECK_TOUCHES(ASM_X86_64("mov 0x20(%rsp), %ah"),
+                {
+                    Stack_Map_Touch::read(0, 0x20, 1),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "mov 0x20(%rsp), %ah"
-  ), {
-    Stack_Map_Touch::read(0, 0x20, 1),
-  });
+  CHECK_TOUCHES(ASM_X86_64("movzbl 0x20(%rsp), %eax"),
+                {
+                    Stack_Map_Touch::read(0, 0x20, 1),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "movzbl 0x20(%rsp), %eax"
-  ), {
-    Stack_Map_Touch::read(0, 0x20, 1),
-  });
+  CHECK_TOUCHES(ASM_X86_64("movzbq 0x20(%rsp), %rax"),
+                {
+                    Stack_Map_Touch::read(0, 0x20, 1),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "movzbq 0x20(%rsp), %rax"
-  ), {
-    Stack_Map_Touch::read(0, 0x20, 1),
-  });
+  CHECK_TOUCHES(ASM_X86_64("movzwq 0x20(%rsp), %rax"),
+                {
+                    Stack_Map_Touch::read(0, 0x20, 2),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "movzwq 0x20(%rsp), %rax"
-  ), {
-    Stack_Map_Touch::read(0, 0x20, 2),
-  });
+  CHECK_TOUCHES(ASM_X86_64("movslq 0x20(%rsp), %rax"),
+                {
+                    Stack_Map_Touch::read(0, 0x20, 4),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "movslq 0x20(%rsp), %rax"
-  ), {
-    Stack_Map_Touch::read(0, 0x20, 4),
-  });
+  CHECK_TOUCHES(ASM_X86_64("movsbq 0x20(%rsp), %rbx"),
+                {
+                    Stack_Map_Touch::read(0, 0x20, 1),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "movsbq 0x20(%rsp), %rbx"
-  ), {
-    Stack_Map_Touch::read(0, 0x20, 1),
-  });
+  CHECK_TOUCHES(ASM_X86_64("movswq 0x20(%rsp), %rbx"),
+                {
+                    Stack_Map_Touch::read(0, 0x20, 2),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "movswq 0x20(%rsp), %rbx"
-  ), {
-    Stack_Map_Touch::read(0, 0x20, 2),
-  });
+  CHECK_TOUCHES(ASM_X86_64("cmp 0x20(%rsp), %rbx"),
+                {
+                    Stack_Map_Touch::read(0, 0x20, 8),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "cmp 0x20(%rsp), %rbx"
-  ), {
-    Stack_Map_Touch::read(0, 0x20, 8),
-  });
-
-  CHECK_TOUCHES(ASM_X86_64(
-    "cmp 0x20(%rsp), %bh"
-  ), {
-    Stack_Map_Touch::read(0, 0x20, 1),
-  });
-
-  // clang-format on
+  CHECK_TOUCHES(ASM_X86_64("cmp 0x20(%rsp), %bh"),
+                {
+                    Stack_Map_Touch::read(0, 0x20, 1),
+                });
 }
 
 TEST(Test_ASM_Stack_Map, offset_is_byte_of_start_of_instruction) {
-  // clang-format off
+  CHECK_TOUCHES(ASM_X86_64("nop"
+                           "mov (%rsp), %rax"),
+                {
+                    Stack_Map_Touch::read(1, 0, 8),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "nop"
-    "mov (%rsp), %rax"
-  ), {
-    Stack_Map_Touch::read(1, 0, 8),
-  });
-
-  CHECK_TOUCHES(ASM_X86_64(
-    "test %rax, %rax"
-    "mov (%rsp), %rax"
-  ), {
-    Stack_Map_Touch::read(3, 0, 8),
-  });
-
-  // clang-format on
+  CHECK_TOUCHES(ASM_X86_64("test %rax, %rax"
+                           "mov (%rsp), %rax"),
+                {
+                    Stack_Map_Touch::read(3, 0, 8),
+                });
 }
 
 TEST(Test_ASM_Stack_Map, push_touches_stack) {
-  // clang-format off
+  CHECK_TOUCHES(ASM_X86_64("pushq %rax"), {
+                                              Stack_Map_Touch::write(0, -8, 8),
+                                          });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "pushq %rax"
-  ), {
-    Stack_Map_Touch::write(0, -8, 8),
-  });
+  CHECK_TOUCHES(ASM_X86_64("pushw %bx"), {
+                                             Stack_Map_Touch::write(0, -2, 2),
+                                         });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "pushw %bx"
-  ), {
-    Stack_Map_Touch::write(0, -2, 2),
-  });
-
-  CHECK_TOUCHES(ASM_X86_64(
-    "pushw (%rbp)"
-  ), {
-    Stack_Map_Touch::write(0, -2, 2),
-  });
-
-  // clang-format on
+  CHECK_TOUCHES(ASM_X86_64("pushw (%rbp)"),
+                {
+                    Stack_Map_Touch::write(0, -2, 2),
+                });
 }
 
 TEST(Test_ASM_Stack_Map, push_updates_rsp) {
-  // clang-format off
-
-  CHECK_TOUCHES(ASM_X86_64(
-    "pushq %rax"
-    "movq $69, (%rsp)"
-  ), {
-    Stack_Map_Touch::write(0, -8, 8),
-    Stack_Map_Touch::write(1, -8, 8),
-  });
-
-  // clang-format on
+  CHECK_TOUCHES(ASM_X86_64("pushq %rax"
+                           "movq $69, (%rsp)"),
+                {
+                    Stack_Map_Touch::write(0, -8, 8),
+                    Stack_Map_Touch::write(1, -8, 8),
+                });
 }
 
 TEST(Test_ASM_Stack_Map, pop_updates_rsp) {
-  // clang-format off
+  CHECK_TOUCHES(ASM_X86_64("pop %rax"
+                           "movq $69, (%rsp)"),
+                {
+                    Stack_Map_Touch::read(0, 0, 8),
+                    Stack_Map_Touch::write(1, 8, 8),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "pop %rax"
-    "movq $69, (%rsp)"
-  ), {
-    Stack_Map_Touch::read(0, 0, 8),
-    Stack_Map_Touch::write(1, 8, 8),
-  });
-
-  CHECK_TOUCHES(ASM_X86_64(
-    "pop %di"
-    "movq $69, (%rsp)"
-  ), {
-    Stack_Map_Touch::read(0, 0, 2),
-    Stack_Map_Touch::write(2, 2, 8),
-  });
-
-  // clang-format on
+  CHECK_TOUCHES(ASM_X86_64("pop %di"
+                           "movq $69, (%rsp)"),
+                {
+                    Stack_Map_Touch::read(0, 0, 2),
+                    Stack_Map_Touch::write(2, 2, 8),
+                });
 }
 
 TEST(Test_ASM_Stack_Map, rsp_relative_mov_after_stack_adjustment) {
-  // clang-format off
+  CHECK_TOUCHES(ASM_X86_64("sub $0x50, %rsp"
+                           "mov %rax, 0x30(%rsp)"),
+                {
+                    Stack_Map_Touch::write(4, -0x50 + 0x30, 8),
+                });
 
-  CHECK_TOUCHES(ASM_X86_64(
-    "sub $0x50, %rsp"
-    "mov %rax, 0x30(%rsp)"
-  ), {
-    Stack_Map_Touch::write(4, -0x50 + 0x30, 8),
-  });
-
-  CHECK_TOUCHES(ASM_X86_64(
-    "add $0x50, %rsp"
-    "mov %rax, 0x30(%rsp)"
-  ), {
-    Stack_Map_Touch::write(4, +0x50 + 0x30, 8),
-  });
-
-  // clang-format on
+  CHECK_TOUCHES(ASM_X86_64("add $0x50, %rsp"
+                           "mov %rax, 0x30(%rsp)"),
+                {
+                    Stack_Map_Touch::write(4, +0x50 + 0x30, 8),
+                });
 }
 
 TEST(Test_ASM_Stack_Map, push_after_stack_adjustment) {
-  // clang-format off
-
-  CHECK_TOUCHES(ASM_X86_64(
-    "sub $0x50, %rsp"
-    "pushq (%rdi)"
-  ), {
-    Stack_Map_Touch::write(4, -0x50 - 8, 8),
-  });
-
-  // clang-format on
+  CHECK_TOUCHES(ASM_X86_64("sub $0x50, %rsp"
+                           "pushq (%rdi)"),
+                {
+                    Stack_Map_Touch::write(4, -0x50 - 8, 8),
+                });
 }
 
 TEST(Test_ASM_Stack_Map,
      mov_immediate_to_register_then_adjust_rsp_by_register) {
-  // clang-format off
-
-  CHECK_TOUCHES(ASM_X86_64(
-    "mov $0x50, %rax"
-    "sub %rax, %rsp"
-    "mov %rbx, (%rsp)"
-  ), {
-    Stack_Map_Touch::write(10, -0x50, 8),
-  });
-
-  // clang-format on
+  CHECK_TOUCHES(ASM_X86_64("mov $0x50, %rax"
+                           "sub %rax, %rsp"
+                           "mov %rbx, (%rsp)"),
+                {
+                    Stack_Map_Touch::write(10, -0x50, 8),
+                });
 }
 
 TEST(Test_ASM_Stack_Map, ret_reads_return_address_and_adjusts_stack) {
